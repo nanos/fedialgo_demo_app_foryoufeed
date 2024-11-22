806ba77175c4a7efb8caca0417e956ab04f01cbf
ashariyar <ashariyar@users.noreply.github.com>
2024-11-22 18:49:50 -0500

    install.sh

--- quick_install.sh
+++ quick_install.sh
@@ -5,5 +5,5 @@ set -e
 git clone https://github.com/michelcrypt4d4mus/fedialgo_demo_app_foryoufeed
 cd fedialgo_demo_app_foryoufeed
 npm install
-echo -e "You can ignore warnings above but not errors."
-echo -e "\nRun 'npm run start' to start the app server, then point your browser at http://localhost:3000/"
+echo -e "\n\n\nYou can ignore any warnings above but not errors."
+echo -e "\nRun 'cd fedialgo_demo_app_foryoufeed && npm run start' to start the app server, then point your browser at http://localhost:3000/"
