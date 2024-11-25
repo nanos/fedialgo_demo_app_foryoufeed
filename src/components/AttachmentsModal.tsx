e272a8e059fb6d1ae87a4f4fb1a43dacd309beac
ashariyar <ashariyar@users.noreply.github.com>
2024-11-25 01:22:17 -0500

    modal size

--- src/components/AttachmentsModal.tsx
+++ src/components/AttachmentsModal.tsx
@@ -22,7 +22,7 @@ export default function AttachmentsModal(
     const media = toot.mediaAttachments[mediaInspectionModalIdx];
 
     return (
-        <Modal onHide={() => setMediaInspectionModalIdx(-1)} show={mediaInspectionModalIdx != -1} size='lg'>
+        <Modal fullscreen={'xxl-down'} onHide={() => setMediaInspectionModalIdx(-1)} show={mediaInspectionModalIdx != -1} size='lg'>
             <Modal.Header closeButton>
                 <Modal.Title>
                     {parse(toot.content)[100]}
