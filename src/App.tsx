6503ee36f940f819a6b9c55b3ed7d5edabd2810c
ashariyar <ashariyar@users.noreply.github.com>
2024-11-25 20:26:17 -0500

    Min height for login screen

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App: React.FC = () => {
         <BrowserRouter>
             <AuthProvider>
                 <div
-                    className='container-fluid vh-100'
+                    className='container-fluid min-vh-100'
                     style={{
                         alignItems: 'center',
                         backgroundColor: 'black',
