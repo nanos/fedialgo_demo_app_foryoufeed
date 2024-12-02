cb30e18c5a4dfb3e04652333bcf9379a7a7c088e
ashariyar <ashariyar@users.noreply.github.com>
2024-12-02 18:36:42 -0500

    bump script

--- bump_fedialgo_commit_hash.sh
+++ bump_fedialgo_commit_hash.sh
@@ -5,10 +5,11 @@ set -e
 pushd ../fedialgo
 git push
 FEDIALGO_COMMIT_HASH=$(git log -1 --format=%H)
-echo "Got FEDIALGO_COMMIT_HASH: $FEDIALGO_COMMIT_HASH"
+echo -e "\n\nGot FEDIALGO_COMMIT_HASH: $FEDIALGO_COMMIT_HASH"
 npm link
 
 popd
+echo -e "\nRunning 'npm install github:michelcrypt4d4mus/fedialgo#${FEDIALGO_COMMIT_HASH}'..."
 npm install github:michelcrypt4d4mus/fedialgo#${FEDIALGO_COMMIT_HASH}
 git commit -am "Bump fedialgo commit hash to $FEDIALGO_COMMIT_HASH"
 git push
