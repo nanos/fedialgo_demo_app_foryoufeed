77d15564e739871d5f3fca9ad5fa08000a0b6578
ashariyar <ashariyar@users.noreply.github.com>
2025-04-18 19:23:15 -0400

    tag script

--- tag_release.sh
+++ tag_release.sh
@@ -12,23 +12,29 @@ assert_repo_is_ready() {
 }
 
 
+tag_repo() {
+    local version_number="v$1"
+    local repo_dir=`basename $PWD`
+    echo "Tagging repo $repo_dir with version: $version_number"
+    git push origin "$MASTER_BRANCH"
+    git tag "$version_number"
+    git push origin "$version_number"
+}
+
+
 # Set vars
+assert_repo_is_ready
 MASTER_BRANCH="master"
 VERSION_NUMBER=`git_get_tag_version`
-assert_repo_is_ready  # Check this dir is clean before we start tagging etc
 
 # Handle fedialgo dir
 pushd ../fedialgo
 assert_repo_is_ready
 update_changelog "$VERSION_NUMBER"
 git commit -am"Bump last CHANGELOG.md version to $VERSION_NUMBER"
-git push origin "$MASTER_BRANCH"
-git tag "$VERSION_NUMBER"
-git push origin "$VERSION_NUMBER"
+tag_repo "$VERSION_NUMBER"
 
 # Handle demo app dir (this dir)
 popd
 ./bump_fedialgo_commit_hash.sh   # This will execute a commit
-git push origin "$MASTER_BRANCH"
-git tag "$VERSION_NUMBER"
-git push origin "$VERSION_NUMBER"
+tag_repo "$VERSION_NUMBER"
