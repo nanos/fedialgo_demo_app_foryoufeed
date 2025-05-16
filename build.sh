c2b13795642b794555c6faab6c37f939b968b737
ashariyar <ashariyar@users.noreply.github.com>
2025-05-16 03:07:56 -0400

    build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 mv .env .env.development  # Don't want these variables in the build
 git checkout github_pages
 git merge master
-NODE_ENV=production FEDIALGO_DEBUG=false QUICK_MODE=false npx webpack --mode production
+NODE_ENV=production npx webpack --mode production
 git commit -am"Build"
 git push origin github_pages
 git checkout master
